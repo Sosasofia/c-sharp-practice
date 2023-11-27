26f36674517a62cf41194c4324df5cb5d1b0056b
Sosasofia <80438887+Sosasofia@users.noreply.github.com>
2023-11-26 21:11:52 -0300

    #2 - Get started with c#
    Upload completed projects

---
+++ Learn Microsoft/Part 2 - Create and run console apps/Challenge Project/Challenge Project/Program.cs
@@ -0,0 +1,119 @@
+﻿// initialize variables - graded assignments 
+int examAssignments = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+
+// Student names
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+int[] studentScores = new int[10];
+
+string currentStudentLetterGrade = "";
+
+Console.WriteLine("Student\t\tExam Score\tOverall\tGrade\tExtra Credit\n");
+
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+    else
+        continue;
+
+
+    // initialize/reset the sum of scored assignments
+    int sumAssignmentScores = 0;
+    int sumExtraScores = 0;
+
+
+    // initialize/reset the calculated average of exam + extra credit scores
+    decimal currentStudentGrade = 0;
+    decimal currentStudentExamScore = 0;
+    decimal currentStudentExtraCreditScore = 0;
+
+    // initialize/reset a counter for the number of assignments
+    int gradedAssignments = 0;
+    int extraScoreAssignments = 0;
+
+    foreach (int score in studentScores)
+    {
+        // increment the assignment counter
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments)
+        {
+            // add the exam score to the sum
+            sumAssignmentScores += score;
+        }
+
+        else
+        {
+            // count extra assignments done
+            extraScoreAssignments += 1;
+            sumExtraScores += score;
+        }
+
+    }
+
+
+    currentStudentExamScore = (decimal)(sumAssignmentScores) / examAssignments;
+    currentStudentExtraCreditScore = (decimal)(sumExtraScores) / extraScoreAssignments;
+
+    currentStudentGrade = (decimal)((decimal)sumAssignmentScores + ((decimal)sumExtraScores / 10)) / examAssignments;
+
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentExamScore}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{currentStudentExtraCreditScore} ({(((decimal)sumExtraScores / 10) / examAssignments)} pts)");
+}
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
